b0d8e8fb136129b7de28895ac1bef8d6e2b01bcf
ukxd <127911083+ukxd@users.noreply.github.com>
2023-03-15 02:13:03 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=0de9c125-4ca5-4c5b-890b-aee8aee9adda
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.holehike.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=443
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
